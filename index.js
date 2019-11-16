83b20029042e5fc2758a532b87d90ae86f327b77
CurtisMIT <curtismitmit@gmail.com>
2019-11-16 16:19:27 -0500

    a sample game

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const express = require('express')
 const app = express()
 
-const port = 3000
-app.use(express.static('public'))
-app.listen(port, () => console.log(`Server listening on ${port}!`))
+// const port = 3000
+// app.use(express.static('public'))
+// app.listen(port, () => console.log(`Server listening on ${port}!`))
+
+const sitePath = process.argv[2] || ".";
+const port = 4242;
+ 
+// Request logging
+app.use(function(req, res, next) {
+    console.log(req.url);
+    next();
+});
+ 
+// Start server
+console.log(sitePath);
+console.log("Starting server in: " + __dirname + '/' + sitePath);
+app.use(express.static(__dirname + '/' + sitePath));
+app.listen(port, function() { 
+    console.log("Server running at: http://localhost:" + port)
+});
