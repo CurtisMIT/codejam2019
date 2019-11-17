53a384db3dcfcfa7c72e509b31fdaa6681b1d470
CurtisMIT <curtismitmit@gmail.com>
2019-11-16 19:57:36 -0500

    moving conveyor belt with a clickable trainer

--- public/scripts/start.js
+++ public/scripts/start.js
@@ -16,17 +16,36 @@ const config = {
 }
 const game = new Phaser.Game(config)
 let trainer
+let direction
+let platforms
 let cursors
 function preload() {
     this.load.image('sky', 'assets/sky.png')
     this.load.spritesheet('trainer', 'assets/trainer.png', { frameWidth: 51, frameHeight: 54 })
+    this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 })
+    this.load.image('star', 'assets/star.png')
+    this.load.spritesheet('belt', 'assets/belt.png', { frameWidth: 383.75, frameHeight: 37})
+}
+
+function listener() {
+    console.log('clicked')
 }
 
 function create() {
     this.add.image(400, 300, 'sky')
+    // this.add.image(400, 200, 'belt')
+    // platforms = this.physics.add.staticGroup()
+    // platforms.create(400, 568, 'belt').setScale(2).refreshBody()
+    // platforms.create(400, 568, 'temp').setScale(2).refreshBody()
+    belt = this.physics.add.sprite(400, 580, 'belt')
+    belt.setScale(2.5)
+    belt.body.static = true
 
     trainer = this.physics.add.sprite(100, 450, 'trainer')
     trainer.setCollideWorldBounds(true)
+    trainer.setInteractive()
+    trainer.on('pointerdown', listener)
+
 
     this.anims.create({
         key: 'still',
@@ -62,6 +81,13 @@ function create() {
         repeat: -1
     })
 
+    this.anims.create({
+        key: 'belt',
+        frames: this.anims.generateFrameNumbers('belt', { start: 3, end: 0 }),
+        frameRate: 30,
+        repeat: -1
+    })
+
     cursors = this.input.keyboard.createCursorKeys()
 
 }
@@ -72,25 +98,43 @@ function update() {
         trainer.setVelocityY(0)
 
         trainer.anims.play('west', true)
+        direction = 'west'
     } else if (cursors.right.isDown) {
         trainer.setVelocityX(160)
         trainer.setVelocityY(0)
         
         trainer.anims.play('east', true)
+        direction = 'east'
     } else if (cursors.up.isDown) {
         trainer.setVelocityX(0)
         trainer.setVelocityY(-160)
 
         trainer.anims.play('north', true)
+        direction = 'north'
     } else if (cursors.down.isDown) {
         trainer.setVelocityX(0)
         trainer.setVelocityY(160)
 
         trainer.anims.play('south', true)
+        direction = 'south'
     } else {
         trainer.setVelocityX(0)
         trainer.setVelocityY(0)
 
-        trainer.anims.play('still')
+        // if (direction === 'west') {
+        //     trainer.anims.play('west')
+        // } else if (direction === 'east') {
+        //     trainer.anims.play('east')
+        // } else if (direction === 'north') {
+        //     trainer.anims.play('north')
+        // } else if (direction === 'south') {
+        //     trainer.anims.play('still')
+        // } else {
+        //     trainer.anims.play('still')
+        // }
+
+        trainer.anims.play('north', true)
+        // platforms.anims.play('belt', true)
+        belt.anims.play('belt', true)
     }
 }
